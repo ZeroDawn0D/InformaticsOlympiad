fac60fd8dd0749f8fa34eee062bc7c4c5cf229db
Umang Majumder <54509868+ZeroDawn0D@users.noreply.github.com>
2020-06-20 11:28:39 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define NL cout<<'\n';
+#define fast_io cin.tie(0);cout.tie(0); ios_base::sync_with_stdio(0)
+#define INF 100000000
+#define unordered_map umap
+#define unordered_set uset
+#define debug(x) cout<<x<<' '
+#define debugNL(x) cout<<x<<'\n'
+
+using namespace std;
+
+void prog()
+{
+
+}
+
+int main()
+{
+    fast_io;
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w". stdout);
+    #endif
+    int t = 1;
+    cin>>t;
+    while(t>0)
+    {
+        prog();
+        t--;
+    }
+    return 0;
+}
